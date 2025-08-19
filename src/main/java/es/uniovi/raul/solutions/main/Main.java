0f7a15625408c0df5877229d858516597032903b
Raúl Izquierdo Castanedo <raul.izquierdo@gmail.com>
2025-08-19 03:08:48 +0200

    refac: merge Main and App
    App hadn't code to be tested, just set up things. So it served the same purpose as Main

--- src/main/java/es/uniovi/raul/solutions/main/Main.java
+++ src/main/java/es/uniovi/raul/solutions/main/Main.java
@@ -1,12 +1,16 @@
 package es.uniovi.raul.solutions.main;
 
-import java.util.Optional;
-
-import es.uniovi.raul.solutions.cli.*;
-
 import static es.uniovi.raul.solutions.cli.Console.*;
 
+import java.io.IOException;
 import java.time.Clock;
+import java.util.*;
+
+import es.uniovi.raul.solutions.cli.*;
+import es.uniovi.raul.solutions.course.*;
+import es.uniovi.raul.solutions.github.GithubConnection.*;
+import es.uniovi.raul.solutions.github.GithubConnectionImpl;
+import es.uniovi.raul.solutions.schedule.ScheduleLoader.InvalidScheduleFormat;
 
 /**
  * Entry point for the application.
@@ -23,19 +27,54 @@ public class Main {
 
         int exitCode = 0;
         try {
-            var app = new App(Clock.systemDefaultZone(), Console::confirmation);
-            exitCode = app.run(argumentsOpt.get());
+
+            exitCode = run(argumentsOpt.get());
 
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             printError("Operation was interrupted.");
             exitCode = 1;
-
         } catch (Exception e) {
             printError(e.getMessage());
             exitCode = 1;
         }
-
         System.exit(exitCode);
+    }
+
+    static int run(Arguments arguments) throws IOException, InvalidScheduleFormat, UnexpectedFormatException,
+            RejectedOperationException, InterruptedException {
+
+        var schedule = loadSchedule(arguments.scheduleFile);
+
+        System.out.print("Connecting with Github... ");
+        var connection = new GithubConnectionImpl(arguments.token);
+        var course = new Course(arguments.organization, connection, schedule);
+        System.out.println("done.\n");
+
+        Prompter prompter = Console::confirmation;
+        var suggester = new AutoGrantAgent(Clock.systemDefaultZone(), prompter);
+        if (suggester.tryAutomaticSelection(course))
+            return 0;
+
+        var manualAgent = new ManualGrantAgent(prompter);
+        manualAgent.doManualSelection(course);
+
+        return 0;
+    }
+
+    private static Map<String, Schedule> loadSchedule(String scheduleFile)
+            throws IOException, es.uniovi.raul.solutions.schedule.ScheduleLoader.InvalidScheduleFormat {
+
+        if (scheduleFile == null) {
+            Console.printWarning("No schedule file specified, so automatic group detection will be disabled.");
+            return Collections.emptyMap();
+        }
+
+        var schedules = es.uniovi.raul.solutions.schedule.ScheduleLoader.load(scheduleFile);
+        if (schedules.isEmpty())
+            throw new es.uniovi.raul.solutions.schedule.ScheduleLoader.InvalidScheduleFormat(
+                    scheduleFile + " is empty.");
+
+        return schedules;
     }
 }
